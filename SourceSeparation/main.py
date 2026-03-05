a5e2a39dea0ad51f702c27e152d57bd262faa049
inesbroto <49390205+inesbroto@users.noreply.github.com>
2026-03-05 18:50:19 +0100

    compute ss from list of files, not df

--- SourceSeparation/main.py
+++ SourceSeparation/main.py
@@ -3,10 +3,9 @@ import pandas as pd
 from SourceSeparator import DemucsSeparator
 import torch
 import time
+from pathlib import Path
 
 time_0 = time.time()
-
-df = pd.read_csv("/home/ibroto/Documents/UPF_SMC/MIR/flamenco-mir/SourceSeparation/wav_f0_paths_mapping.csv")  # contains column "path"
 
 print(f"CUDA available? {torch.cuda.is_available()}")
 
@@ -15,7 +14,18 @@ separator = DemucsSeparator(
     device="cuda" if torch.cuda.is_available() else "cpu"
 )
 
-separator.separate_from_dataframe(df[:2], "wav_path")
+folder = Path("/home/ibroto/Documents/UPF_SMC/MIR/flamenco-mir/data/cante2midiaudio")
+wav_files = list(folder.glob("*.wav"))
 
+for audio_file in wav_files:
+    file_start_time = time.time()
+    print(f"Processing {audio_file}")
+    separator.separate_file(audio_file)
+    print(f"File processed in {time.time()-file_start_time} seconds.")
+
+
+## to separate from df column
+##df = pd.read_csv("/home/ibroto/Documents/UPF_SMC/MIR/flamenco-mir/SourceSeparation/wav_f0_paths_mapping.csv")  # contains column "path"
+#separator.separate_from_dataframe(df[:2], "wav_path")
 
 print(f"Ellapsed_time: {time.time()-time_0}")
