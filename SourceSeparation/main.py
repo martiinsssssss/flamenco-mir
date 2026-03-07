0e391becc94cbdb6ae32da943720bbc7a15ded01
inesbroto <49390205+inesbroto@users.noreply.github.com>
2026-03-07 12:58:24 +0100

    add if __name__ == "__main__"

--- SourceSeparation/main.py
+++ SourceSeparation/main.py
@@ -5,27 +5,30 @@ import torch
 import time
 from pathlib import Path
 
-time_0 = time.time()
 
-print(f"CUDA available? {torch.cuda.is_available()}")
+if __name__ == "__main__":
 
-separator = DemucsSeparator(
-    output_dir="demucs_output",
-    device="cuda" if torch.cuda.is_available() else "cpu"
-)
+    time_0 = time.time()
 
-folder = Path("/home/ibroto/Documents/UPF_SMC/MIR/flamenco-mir/data/cante2midiaudio")
-wav_files = list(folder.glob("*.wav"))
+    print(f"CUDA available? {torch.cuda.is_available()}")
 
-for audio_file in wav_files:
-    file_start_time = time.time()
-    print(f"Processing {audio_file}")
-    separator.separate_file(audio_file)
-    print(f"File processed in {time.time()-file_start_time} seconds.")
+    separator = DemucsSeparator(
+        output_dir="demucs_output",
+        device="cuda" if torch.cuda.is_available() else "cpu"
+    )
+
+    folder = Path("/home/ibroto/Documents/UPF_SMC/MIR/flamenco-mir/data/cante2midiaudio")
+    wav_files = list(folder.glob("*.wav"))
+
+    for audio_file in wav_files:
+        file_start_time = time.time()
+        print(f"Processing {audio_file}")
+        separator.separate_file(audio_file)
+        print(f"File processed in {time.time()-file_start_time} seconds.")
 
 
-## to separate from df column
-##df = pd.read_csv("/home/ibroto/Documents/UPF_SMC/MIR/flamenco-mir/SourceSeparation/wav_f0_paths_mapping.csv")  # contains column "path"
-#separator.separate_from_dataframe(df[:2], "wav_path")
+    ## to separate from df column
+    ##df = pd.read_csv("/home/ibroto/Documents/UPF_SMC/MIR/flamenco-mir/SourceSeparation/wav_f0_paths_mapping.csv")  # contains column "path"
+    #separator.separate_from_dataframe(df[:2], "wav_path")
 
-print(f"Ellapsed_time: {time.time()-time_0}")
+    print(f"Ellapsed_time: {time.time()-time_0}")
